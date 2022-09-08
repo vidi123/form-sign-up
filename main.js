57e1806a99e7016e6435a1e633831aee75ca3b59
vidi123 <fadil.arofah79@gmail.com>
2022-09-08 22:56:37 +0700

    merapihkan form pada index.html dan main.js

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ var valid = function() {
     if(document.getElementById('password').value == document.getElementById('password2').value) {
         document.getElementById('confirm').innerHTML = ''; 
         document.getElementById('submit').type = 'submit';
-
     } else {
         document.getElementById('confirm').innerHTML = '*Password tidak sesuai'; 
         document.getElementById('submit').type = 'button';
     }
-    return valid;
 }
